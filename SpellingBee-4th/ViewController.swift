db371e0aca9db8f258c8649ef207a9d9cf661bfb
Eric Hernandez <eands9@yahoo.com>
2018-11-30 14:14:33 -0600

    Initial Commit

---
+++ SpellingBee-4th/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpellingBee-4th
+//
+//  Created by Eric Hernandez on 11/30/18.
+//  Copyright © 2018 Eric Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
