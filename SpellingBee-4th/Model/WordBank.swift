6d303c11c95c110f1b6d1f97b6f99a293491b4b2
Eric Hernandez <eands9@yahoo.com>
2018-11-30 23:13:14 -0600

    fix sentence

--- SpellingBee-4th/Model/WordBank.swift
+++ SpellingBee-4th/Model/WordBank.swift
@@ -13,20 +13,20 @@ class WordBank{
     
     init(){
         list.append(Word(word:"mirror", sentence:"My mirror broke into tiny pieces."))
-        list.append(Word(word:"resemble", sentence:"I resemble my mother."))
-        list.append(Word(word:"citizen", sentence:"Citizens are everywhere in New York."))
+        list.append(Word(word:"resemble", sentence:"With my blue eyes, I resemble my mother."))
+        list.append(Word(word:"citizen", sentence:"A great citizen should be remembered."))
         list.append(Word(word:"pronounce", sentence:"My friend cannot pronounce the word rabbit."))
         list.append(Word(word:"taut", sentence:"Her taut muscles relaxed and she felt weak."))
         list.append(Word(word:"monopoly", sentence:"His friend likes the game monopoly."))
         list.append(Word(word:"florence", sentence:"My older cousins name is florence."))
         list.append(Word(word:"leopard", sentence:"Her favorite animal is a leopard."))
-        list.append(Word(word:"comrades", sentence:"Before long, his old comrade arrived."))
+        list.append(Word(word:"comrades", sentence:"Before long, his old comrades arrived."))
         list.append(Word(word:"swiveled", sentence:" She swiveled in her seat to check the time."))
-        list.append(Word(word:"borneo", sentence:"His dad's name is borneo."))
-        list.append(Word(word:"scaffolding", sentence:"He met his death on the scaffold with calmness and dignity."))
+        list.append(Word(word:"borneo", sentence:"Borneo is a beautiful country."))
+        list.append(Word(word:"scaffolding", sentence:"An aging scaffolding led to his fall"))
         list.append(Word(word:"warped", sentence:"His critical sense was, perhaps, somewhat warped."))
-        list.append(Word(word:"flail", sentence:"She flailed her arms to get the bugs away from her face. "))
-        list.append(Word(word:"clanging", sentence:"The carolers were clanging there bells."))
+        list.append(Word(word:"flail", sentence:"Flail your arms to get the bugs away from you. "))
+        list.append(Word(word:"clanging", sentence:"The carolers were clanging their bells."))
         list.append(Word(word:"qualify", sentence:"I'm trying to qualify for the spelling bee."))
     }
     
